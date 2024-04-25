5fccd936640c82360c78333c5addec3f079614b9
nvkudrin <85198409+nvkudrin@users.noreply.github.com>
2024-04-25 12:49:07 -0400

    Add files via upload

--- PiGamma.m
+++ PiGamma.m
@@ -1,5 +1,6 @@
 function [LogLikelihood] = PiGamma(X, a, b)
-
+%This function calculates the log-likelihood function for estimating
+%parameters of Pi = Gamma(a,b) distribution
 l = zeros(size(X));
 h = exp(linspace(-15, 3, 1000));
 Xs = linspace(-10,10, 1000)';
@@ -11,12 +12,6 @@ Is = trapz(Xs,Is);
 Mx = normpdf(X-h);
 Mh = repmat(gampdf(h, a, b), length(X), 1);
 I = trapz(h, Mx.*Mh, 2)/Is;
-% parfor j = 1:length(X)
-%     %I = integral(@(h) (normpdf(X(j)-h)).*gampdf(h, a, b), 0, Inf);
-%     I = (normpdf(X(j)-h)).*gampdf(h, a, b);
-%     I = trapz(h, I);
-%     l(j) = log(I);
-% end
 LogLikelihood = sum(log(I));
 end
 
