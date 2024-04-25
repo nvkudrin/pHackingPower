aa65963361122054e5556d99f2187aaa78356ac9
nvkudrin <85198409+nvkudrin@users.noreply.github.com>
2024-04-25 12:44:32 -0400

    Add files via upload

--- EdgesN.m
+++ EdgesN.m
@@ -1,4 +1,5 @@
 function [edges0, N0] = EdgesN(P0)
+% this function generates histogram versions of the Monte Carlo p-curves
 edges = linspace(0,1, 401);
 [N0,edges0] = histcounts(P0, edges, 'Normalization','pdf');
 edges0 = edges0(2:end) - (edges0(2)-edges0(1))/2;
